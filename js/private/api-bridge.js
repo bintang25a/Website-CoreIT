e28a9acbaa92334fc04c4bde975cd0d474437eb8
bintang25a <135932300+bintang25a@users.noreply.github.com>
2025-01-11 12:57:15 +0700

    Update api-bridge.js

--- js/private/api-bridge.js
+++ js/private/api-bridge.js
@@ -9,9 +9,9 @@ export async function getToken() {
 export async function getMember() {
     const token = localStorage.getItem('token');
 
-    // if (!token) {
-    //     getToken();
-    // }
+    if (!token) {
+        getToken();
+    }
 
     const response = await fetch('https://coreit-database.vercel.app/data/members.js', {
         method: 'GET',
@@ -22,12 +22,13 @@ export async function getMember() {
     });
     
     if (!response.ok) {
+        getToken();
         throw new Error('Failed to fetch members');
         // location.reload(true);
     }
-    // else {
-    //     getToken();
-    // }
+    else {
+        getToken();
+    }
 
     return await response.json();
 }
