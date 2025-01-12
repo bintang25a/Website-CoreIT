fa729d6a1a5eb41478301227b6d83b85deb2687b
bintang25a <135932300+bintang25a@users.noreply.github.com>
2025-01-12 21:32:51 +0700

    Update api-bridge.js

--- js/private/api-bridge.js
+++ js/private/api-bridge.js
@@ -35,16 +35,31 @@ export async function saveMember(newMember) {
         },
         body: JSON.stringify(newMember)
     });
-    
-    const responseBody = await response.text();
-    console.log('Response body:', responseBody);
-
-    try {
-        return JSON.parse(responseBody);
-    } 
-    catch (error) {
-        throw new Error('Failed to parse JSON from response');
-    }
 
     return await response.json();
 }
+
+// export async function saveMember(newMember) {
+//     const token = await getToken();
+
+//     const response = await fetch('https://coreit-database.vercel.app/data/save-member.js', {
+//         method: 'POST',
+//         headers: {
+//             'authorization': `Bearer ${token}`,
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(newMember)
+//     });
+    
+//     const responseBody = await response.text();
+//     console.log('Response body:', responseBody);
+
+//     try {
+//         return JSON.parse(responseBody);
+//     } 
+//     catch (error) {
+//         throw new Error('Failed to parse JSON from response');
+//     }
+
+//     return await response.json();
+// }
